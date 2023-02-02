27399927a5e4ed64cb9eaee5d46fb58c440ea0bc
Hemanta Gayen <69314427+hemucode@users.noreply.github.com>
2023-02-02 11:48:07 +0530

    ok

--- Chrome/data/interface/index.js
+++ Chrome/data/interface/index.js
@@ -22,11 +22,7 @@ function translate() {
  * @returns Promise
  */
 async function hydrate() {
-  // Get settings
-  // const { enabled, videoCount } = await chrome.storage.sync.get({
-  //   enabled: true,
-  //   videoCount: 0,
-  // });
+
    var a = new Promise(function(resolve, reject){
         chrome.storage.sync.get({"enabled": true}, function(options){
             resolve(options.enabled);
@@ -36,40 +32,21 @@ async function hydrate() {
   const enabled = await a;
   console.log(enabled);
 
-  var c = new Promise(function(resolve, reject){
-        chrome.storage.sync.get({"videoCount": 0}, function(options){
-            resolve(options.videoCount);
-        })
-    });
-
-  const videoCount = await c;
-  console.log(videoCount);
 
   // Hydrate Logo
   const $logo = document.querySelector(".logo");
   $logo.style.filter = enabled ? "grayscale(0)" : "grayscale(100%)";
   $logo.style.opacity = enabled ? "1" : "0.7";
 
-  // Hydrate Timesave info
-  const $timeSaveInfo = document.querySelector(".timesave-info");
-  const adTimePerVideo = 0.5;
-  const timeSaved = Math.ceil(videoCount * adTimePerVideo);
-  $timeSaveInfo.textContent = chrome.i18n.getMessage("timesaveInfo", [
-    new Intl.NumberFormat(undefined, {
-      style: "unit",
-      unit: "minute",
-      unitDisplay: "long",
-    }).format(timeSaved),
-  ]);
 
   // Hydrate Checkbox Label
-  const $checkboxLabel = document.querySelector("[data-message=enabled]");
+  const $checkboxLabel = document.querySelector("[data-message=adsblock]");
   $checkboxLabel.textContent = chrome.i18n.getMessage(
-    enabled ? "enabled" : "disabled"
+    enabled ? "adsblock" : "adsunblock"
   );
 
   // Hydrate Checkbox Label
-  const $enabledCheckbox = document.querySelector("input[name=enabled]");
+  const $enabledCheckbox = document.querySelector("input[name=ads]");
   $enabledCheckbox.checked = enabled;
   $enabledCheckbox.addEventListener("change", async (event) => {
     const enabled = event.currentTarget.checked;
@@ -79,13 +56,79 @@ async function hydrate() {
 
     // Update Checkbox Label
     $checkboxLabel.textContent = chrome.i18n.getMessage(
-      enabled ? "enabled" : "disabled"
+      enabled ? "adsblock" : "adsunblock"
     );
 
     // Update Logo
     $logo.style.filter = enabled ? "grayscale(0)" : "grayscale(100%)";
     $logo.style.opacity = enabled ? "1" : "0.7";
   });
+
+
+  var c = new Promise(function(resolve, reject){
+        chrome.storage.sync.get({"videolike": true}, function(options){
+            resolve(options.videolike);
+        })
+    });
+
+  const videolike = await c;
+  console.log(videolike);
+
+
+
+  // Hydrate Checkbox Label
+  const $checkboxLabel_2 = document.querySelector("[data-message=autolike]");
+  $checkboxLabel_2.textContent = chrome.i18n.getMessage(
+    videolike ? "autolike" : "manuallike"
+  );
+
+  // Hydrate Checkbox Label
+  const $enabledCheckbox_2 = document.querySelector("input[name=like]");
+  $enabledCheckbox_2.checked = videolike;
+  $enabledCheckbox_2.addEventListener("change", async (event) => {
+    const videolike = event.currentTarget.checked;
+
+    // Persist
+    await chrome.storage.sync.set({ videolike });
+
+    // Update Checkbox Label
+    $checkboxLabel_2.textContent = chrome.i18n.getMessage(
+      videolike ? "autolike" : "manuallike"
+    );
+  });
+
+
+
+  var d = new Promise(function(resolve, reject){
+        chrome.storage.sync.get({"videosubscribe": true}, function(options){
+            resolve(options.videosubscribe);
+        })
+  });
+
+  const videosubscribe = await d;
+  console.log(videosubscribe);
+
+  const $checkboxLabel_S = document.querySelector("[data-message=autosubscribe]");
+  $checkboxLabel_S.textContent = chrome.i18n.getMessage(
+    videosubscribe ? "autosubscribe" : "manualsubscribe"
+  );
+
+  // Hydrate Checkbox Label
+  const $enabledCheckbox_S = document.querySelector("input[name=subscribe]");
+  $enabledCheckbox_S.checked = videosubscribe;
+  $enabledCheckbox_S.addEventListener("change", async (event) => {
+    const videosubscribe = event.currentTarget.checked;
+
+    // Persist
+    await chrome.storage.sync.set({ videosubscribe });
+
+    // Update Checkbox Label
+    $checkboxLabel_S.textContent = chrome.i18n.getMessage(
+      videosubscribe ? "autosubscribe" : "manualsubscribe"
+    );
+  });
+  document.querySelector(".teaser").href = `https://chrome.google.com/webstore/detail/${chrome.runtime.id}`
+
 }
 
 init();
--- Chrome/lib/service.js
+++ Chrome/lib/service.js
@@ -14,20 +14,6 @@ chrome.runtime.onStartup.addListener(async () => {
   }
 });
 
-chrome.runtime.onInstalled.addListener(async (details) => {
-  switch (details.reason) {
-    case chrome.runtime.OnInstalledReason.INSTALL:
-      return chrome.storage.sync.set({
-        installDate: Date.now(),
-        installVersion: chrome.runtime.getManifest().version,
-      });
-    case chrome.runtime.OnInstalledReason.UPDATE:
-      return chrome.storage.sync.set({
-        updateDate: Date.now(),
-      });
-  }
-});
-
 chrome.runtime.onMessage.addListener(async (request, sender) => {
   switch (request.action) {
     case "INSERT_CSS_RULE": {
@@ -42,6 +28,22 @@ chrome.runtime.onMessage.addListener(async (request, sender) => {
 });
 
 
+chrome.runtime.onInstalled.addListener(async (details) => {
+  switch (details.reason) {
+    case chrome.runtime.OnInstalledReason.INSTALL:
+      return chrome.storage.sync.set({
+        installDate: Date.now(),
+        installVersion: chrome.runtime.getManifest().version,
+      });
+    case chrome.runtime.OnInstalledReason.UPDATE:
+      return chrome.storage.sync.set({
+        updateDate: Date.now(),
+      });
+  }
+});
+
+
+
 chrome.storage.onChanged.addListener(async (changes, namespace) => {
   if (namespace !== "sync") return;
 
@@ -52,73 +54,43 @@ chrome.storage.onChanged.addListener(async (changes, namespace) => {
       await disable();
     }
   }
+});
 
-  if (changes.displayActionCountAsBadgeText) {
-    await chrome.declarativeNetRequest.setExtensionActionOptions({
-      displayActionCountAsBadgeText:
-        changes.displayActionCountAsBadgeText.newValue,
-    });
+chrome.storage.onChanged.addListener(async (changes, namespace) => {
+  if (namespace !== "sync") return;
+
+  if (changes.videolike) {
+    if (changes.videolike.newValue) {
+      await reloadAffectedTab();
+    }else {
+      await reloadAffectedTab();
+    }
+  }
+});
+
+chrome.storage.onChanged.addListener(async (changes, namespace) => {
+  if (namespace !== "sync") return;
+
+  if (changes.videosubscribe) {
+    if (changes.videosubscribe.newValue) {
+      await reloadAffectedTab();
+    }else {
+      await reloadAffectedTab();
+    }
   }
 });
 
 
-chrome.webNavigation.onCompleted.addListener(trackVideoView, {
-  url: [{ hostSuffix: "youtube.com", pathPrefix: "/watch" }],
-});
-
-
-chrome.webNavigation.onHistoryStateUpdated.addListener(trackVideoView, {
-  url: [{ hostSuffix: "youtube.com", pathPrefix: "/watch" }],
-});
-
-/**
- * @returns Promise
- */
-async function trackVideoView() {
-  // const { enabled, videoCount } = await chrome.storage.sync.get({
-  //   enabled: true,
-  //   videoCount: 0,
-  // });
-  var a = new Promise(function(resolve, reject){
-        chrome.storage.sync.get({"enabled": true}, function(options){
-            resolve(options.enabled);
-      })
-  });
-
-  const enabled = await a;
-  console.log(enabled);
-
-  var c = new Promise(function(resolve, reject){
-        chrome.storage.sync.get({"videoCount": 0}, function(options){
-            resolve(options.videoCount);
-        })
-  });
-
-  const videoCount = await c;
-  console.log(videoCount);
-
-  
-
-
-  if (!enabled) return;
-
-  await chrome.storage.sync.set({
-    videoCount: videoCount + 1,
-  });
-}
 
 /**
  * Enables this extension core
  * @returns Promise
  */
+
 async function enable() {
-  await chrome.declarativeNetRequest.updateEnabledRulesets({
-    enableRulesetIds: ["youtube"],
-  });
+
   await chrome.action.setIcon({
     path: {
-      16: "data/icons/icon-16.png",
-      19: "data/icons/icon-19.png",
       32: "data/icons/icon-32.png",
       38: "data/icons/icon-38.png",
       128: "data/icons/icon-128.png",
@@ -130,14 +102,10 @@ async function enable() {
 /**
  * @returns Promise
  */
+
 async function disable() {
-  await chrome.declarativeNetRequest.updateEnabledRulesets({
-    disableRulesetIds: ["youtube"],
-  });
   await chrome.action.setIcon({
     path: {
-      16: "data/icons/icon-disabled-16.png",
-      19: "data/icons/icon-disabled-19.png",
       32: "data/icons/icon-disabled-32.png",
       38: "data/icons/icon-disabled-38.png",
       128: "data/icons/icon-disabled-128.png",
