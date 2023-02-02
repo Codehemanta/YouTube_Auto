4b2aa49e61f4cfe497663492a4b9e06834a8e593
Hemanta Gayen <69314427+hemucode@users.noreply.github.com>
2023-02-02 16:57:07 +0530

    h

---
+++ Morzila/scripts/inject.js
@@ -0,0 +1,7 @@
+//YouTube Auto by @thegayen - https://github.com/Codehemanta/YouTube_Auto/ - @YoutubeAuto
+//License - https://github.com/Codehemanta/YouTube_Auto/blob/main/LICENSE ( JS: Mozilla Public License 2.0 )
+console.log("ok")
+alert("hd") 
+    chrome.storage.sync.set({'foo': 'hello', 'bar': 'hi'}, function() {
+      console.log('Settings saved');
+    });
